200f51c2f451d8787ac428a3639a62a7c030b2db
Gilbert Gerhold <pavel.i.shatalov@gmail.com>
2019-05-04 17:59:17 +0300

    Revert "update libft"
    This reverts commit 57ded406f2ce20f7b189cd2e2f13708aa9562b7b.

--- src/ft_strnew.c
+++ src/ft_strnew.c
@@ -1,22 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ggerhold <ggerhold@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/06 14:28:22 by ggerhold          #+#    #+#             */
-/*   Updated: 2019/05/04 16:03:54 by ggerhold         ###   ########.fr       */
+/*   Created: 2019/05/02 21:46:27 by ggerhold          #+#    #+#             */
+/*   Updated: 2019/05/02 21:46:28 by ggerhold         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "libft.h"
 
-# define BUFF_SIZE 42
+char	*ft_strnew(size_t size)
+{
+	char	*res;
+	size_t	check;
 
-# include "libft/inc/libft.h"
-
-int		get_next_line(const int fd, char **line);
-
-#endif
+	check = 0;
+	if (size == check - 1)
+		return (NULL);
+	res = ft_memalloc((size + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	return (res);
+}
